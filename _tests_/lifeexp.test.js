b8ff3d7237ef1ac2f461ab5922dc95a3a6018f82
Svea Wade <svealinneawade@gmail.com>
2020-11-13 11:56:54 -0800

    good fail on test 2 for life exp

--- _tests_/lifeexp.test.js
+++ _tests_/lifeexp.test.js
@@ -5,4 +5,8 @@ describe ('LifeExp', () => {
     const newlife = new LifeExp(12)
     expect(newlife.mercuryexp).toEqual(6)
   });
+    test('Should correctly return the users life expectancy on Venus', () => {
+      const newlife = new LifeExp(12)
+      expect(newlife.venusexp).toEqual(9)
+    })
 })
