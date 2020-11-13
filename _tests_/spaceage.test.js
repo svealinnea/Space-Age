a872768b91e89c0db0c459866c9797e9bde1c961
Svea Wade <svealinneawade@gmail.com>
2020-11-13 09:57:31 -0800

    test 1 good fail

--- _tests_/spaceage.test.js
+++ _tests_/spaceage.test.js
@@ -1,1 +1,17 @@
-import {} from './../src/space.js';
+import { TestScheduler } from 'jest';
+import {UserAge} from '../src/space.js';
+
+describe ('UserAge', () => {
+  test('should correctly identify a users age', () => {
+    const userage = new UserAge(12)
+    expect(userage.age).toEqual(12);
+  })
+})
+/*
+1. needs to return the users age in Earth Years
+2. should return the users age in Mercury years (.24 Earth Years, 12%.24)
+3. should return the users age in Venus Years (.62 Earth Years 12%.62)
+4. should return the users age in Mars Years (1.88 Earth Years 12% 1.88)
+5. should return the users age in Jupiter Years (11.86 Earth Years 12 % 11.86)
+
+*/
