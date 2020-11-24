6a8f096ba0082a2cd98408bc5ee9fd7081103883
Svea Wade <svealinneawade@gmail.com>
2020-11-24 10:18:28 -0800

    added UI logic and it is working

--- src/main.js
+++ src/main.js
@@ -4,13 +4,16 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import {UserAge} from './space.js';
 
-$('#space').click(function () { 
+$('#space').submit(function () {
+  event.preventDefault();
   let input = $('#input').val();
   let newAge = new UserAge(input);
   console.log(newAge);
-  console.log(newAge.mercury)
+  //console.log(newAge.mercury)
   //mercury = newAge.mercury
-  // let answer = new UserAge(this.jupiter);
-  // console.log(answer);
-  $('#mercury').text()
+  let jupiter = newAge.jupiter
+  let mercury = newAge.mercury
+  let mars = newAge.mars
+  let venus = newAge.venus
+  $('#responses').append(`Your age on Mercury is: ${mercury}, Your age on Jupiter is: ${jupiter}, Your age on Venus is: ${venus}, Your age on Mars is ${mars}`)
 });
